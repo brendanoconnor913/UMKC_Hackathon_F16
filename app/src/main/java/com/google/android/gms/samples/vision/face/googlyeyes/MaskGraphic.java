f8399982426ef21278ae08322283f334a8d9fe7d
Brendan O'Connor <brendan.oconnor.913@gmail.com>
2016-11-13 18:04:06 -0600

    Commit code before refactoring into one project

--- app/src/main/java/com/google/android/gms/samples/vision/face/googlyeyes/MaskGraphic.java
+++ app/src/main/java/com/google/android/gms/samples/vision/face/googlyeyes/MaskGraphic.java
@@ -18,6 +18,7 @@ import com.google.android.gms.vision.face.Landmark;
 import static android.R.attr.x;
 import static android.graphics.Path.Direction.CW;
 import static java.lang.Math.abs;
+import static java.lang.Math.sqrt;
 
 /**
  * Created by brendan on 11/12/16.
@@ -26,6 +27,8 @@ import static java.lang.Math.abs;
 class MaskGraphic extends GraphicOverlay.Graphic {
     private Paint mMaskPaint;
     private Paint mStatusPaint;
+    private Paint mTargetPaint;
+    private Paint mDeviationPaint;
     private Context context;
     private volatile PointF mPosition;
     private float width;
@@ -47,13 +50,21 @@ class MaskGraphic extends GraphicOverlay.Graphic {
         width = 0;
         height = 0;
         mMaskPaint = new Paint();
-        mMaskPaint.setColor(Color.BLACK);
+        mMaskPaint.setColor(Color.WHITE);
         mMaskPaint.setStyle(Paint.Style.STROKE);
         mMaskPaint.setStrokeWidth(15);
         mStatusPaint = new Paint();
         mStatusPaint.setColor(Color.GRAY);
         mStatusPaint.setStyle(Paint.Style.STROKE);
         mStatusPaint.setStrokeWidth(30);
+        mTargetPaint = new Paint();
+        mTargetPaint.setARGB(255, 102,205,170);
+        mTargetPaint.setStyle(Paint.Style.STROKE);
+        mTargetPaint.setStrokeWidth(15);
+        mDeviationPaint = new Paint();
+        mDeviationPaint.setARGB(255, 58,58,60);
+        mDeviationPaint.setStyle(Paint.Style.STROKE);
+        mDeviationPaint.setStrokeWidth(15);
     }
 
     /**
@@ -142,28 +153,30 @@ class MaskGraphic extends GraphicOverlay.Graphic {
         Bitmap bl = BitmapFactory.decodeResource(context.getResources(), R.drawable.lock);
 
         double scaleconst = .66; // need to get working in terms of canvas
+//        if(!canvas.equals(null)) {
+//            Log.i("working", "test");
+//        }
 
         Bitmap smallLock = bl.createScaledBitmap(bl,150,150,true);
-        Bitmap newGlasses = bp.createScaledBitmap(bp, (int)(scaleconst*bp.getWidth()),
-                (int) (scaleconst*bp.getHeight()), true);
-        Bitmap newMustache = bp.createScaledBitmap(bm, (int)(scaleconst*bm.getWidth()),
-                (int) (scaleconst*bm.getHeight()), true);
+//        Bitmap newGlasses = bp.createScaledBitmap(bp, (int)(scaleconst*bp.getWidth()),
+//                (int) (scaleconst*bp.getHeight()), true);
+//        Bitmap newMustache = bp.createScaledBitmap(bm, (int)(scaleconst*bm.getWidth()),
+//                (int) (scaleconst*bm.getHeight()), true);
+//
+//        int gX = (int) ((width - newGlasses.getWidth())/2);
+//        int gY = (int) (3*((height - newGlasses.getHeight())/8));
+//        int mxShift = (int) ((newGlasses.getWidth() - newMustache.getWidth())/2);
+//        int myShift = (int) (1.1 * newGlasses.getHeight());
+//        int mX = gX + mxShift;
+//        int mY = gY + myShift;
 
-        int gX = (int) ((width - newGlasses.getWidth())/2);
-        int gY = (int) (3*((height - newGlasses.getHeight())/8));
-        int mxShift = (int) ((newGlasses.getWidth() - newMustache.getWidth())/2);
-        int myShift = (int) (1.1 * newGlasses.getHeight());
-        int mX = gX + mxShift;
-        int mY = gY + myShift;
-
-        canvas.drawBitmap(newMustache,mX,mY,mMaskPaint);
-        canvas.drawBitmap(newGlasses,gX,gY,mMaskPaint);
-
-        double glasstocan = (newGlasses.getHeight()*newGlasses.getWidth())/(height*width);
-//        Log.i("glass to canvas", Double.toString(glasstocan));
+//
+//
+//        double glasstocan = (newGlasses.getHeight()*newGlasses.getWidth())/(height*width);
+////        Log.i("glass to canvas", Double.toString(glasstocan));
 
         double percentage = (face.getWidth()*face.getHeight())/(canvas.getWidth()*canvas.getHeight());
-        Log.i("percent", Double.toString(percentage));
+//        Log.i("percent", Double.toString(percentage));
         if(percentage >= 0.017 &&  percentage <= .039) {
             timer++;
             if(timer >= 8) {
@@ -181,73 +194,165 @@ class MaskGraphic extends GraphicOverlay.Graphic {
         if(unlocked) {
             canvas.drawBitmap(smallLock, 1100, 50, mMaskPaint);
         }
-//        reMask(canvas, face, bm);
+        drawBounds(canvas,face);
     }
 
-    public void reMask(Canvas canvas, Face face, Bitmap object){
-        //if euler faces are good enough
 
-        double areaFace = face.getWidth()*face.getHeight();
-        double areaScreen = canvas.getWidth()*canvas.getHeight();
-        double beta = areaFace/areaScreen;
+    private void drawBounds(Canvas canvas, Face face){
+        //EulerConstraint
+        if(face.getEulerY() < 10 && face.getEulerY() >-10 &&
+                face.getEulerZ() <10 && face.getEulerZ()>-10){
+            double areaFace = face.getWidth()*face.getHeight();
+            double areaScreen = canvas.getWidth()*canvas.getHeight();
+            double beta = areaFace/areaScreen;
 
-        double alpha = .75;
+            double alpha = .5;
 
-        if(beta == 0) {
-            beta = alpha;
+            //this is in case we get no face on accident
+            if(beta == 0) {
+                beta = alpha;
+            }
+
+            //find the new rectangle bounds for right scale
+            double newWidth = face.getWidth() * sqrt(alpha/beta);
+            double newHeight = face.getHeight() * sqrt(alpha/beta);
+
+            // define threshold values
+            double threshold = .30;
+            double threshLowW = newWidth*Math.sqrt(1-threshold);
+            double threshLowH = newHeight*Math.sqrt(1-threshold);
+            double threshHihW = newWidth*Math.sqrt(1+threshold);
+            double threshHihH = newHeight*Math.sqrt(1+ threshold);
+
+            //draw centered bounds
+            float xMargin = (float)(canvas.getWidth() - newWidth)/2;
+            float yMargin = (float)(canvas.getHeight()- newHeight)/2;
+
+            double topLeftx = xMargin;
+            double topLefty = yMargin;
+
+            double topRightx = xMargin + newWidth;
+            double topRighty = yMargin;
+
+            double bottomLeftx = xMargin;
+            double bottomLefty = yMargin+newHeight;
+
+            double bottomRightx = xMargin + newWidth;
+            double bottomRighty = yMargin + newHeight;
+
+            float xMarginL = (float)(canvas.getWidth() - threshLowW)/2;
+            float yMarginL = (float)(canvas.getHeight()- threshLowH)/2;
+
+            float xMarginH = (float)(canvas.getWidth() - threshHihW)/2;
+            float yMarginH = (float)(canvas.getHeight() - threshHihH)/2;
+
+            Bitmap bm = BitmapFactory.decodeResource(context.getResources(),R.drawable.mustache2);
+            Bitmap bp = BitmapFactory.decodeResource(context.getResources(),R.drawable.glasses);
+
+            Bitmap newMustache = null;
+
+            //rescale moustache
+            //get landmarks
+            PointF LM = null;
+            PointF RM = null;
+            PointF N = null;
+            for(Landmark landmark: face.getLandmarks()){
+                if(landmark.getType() == 5){ //leftmouth
+                    LM = landmark.getPosition();
+                }
+                if(landmark.getType() == 11){ //rightmouth
+                    RM = landmark.getPosition();
+                }
+                if(landmark.getType() == 10) { //nose
+                    N = landmark.getPosition();
+                }
+
+            }
+
+            int gX=0;
+            int gY=0;
+
+
+            float x = translateX(face.getPosition().x + face.getWidth() / 2);
+
+            if(LM != null  && RM != null && N != null) {
+                int newX = (int)(Math.abs(RM.x - LM.x)*Math.sqrt(alpha/beta));
+                int newY = (int)(Math.abs(LM.y - N.y)*Math.sqrt(alpha/beta));
+                newMustache = bp.createScaledBitmap(bm, newX,
+                        newY, true);
+
+                float faceX = translateX(face.getPosition().x);
+                float faceXEnd = translateX(face.getPosition().x + face.getWidth());
+                float faceY = translateY(face.getPosition().y);
+                float faceYEnd = translateY(face.getPosition().y + face.getHeight());
+
+                float noseY = translateY(N.y);
+                float lmX = translateX(LM.x);
+                float rmX = translateX(RM.x);
+                float mX = (lmX + rmX)/2;
+
+                gX = (int) (xMargin + Math.abs((faceXEnd-lmX)/(faceXEnd - faceX))*newWidth);
+                gY = (int) ((yMargin + Math.abs((noseY-faceY)/(faceYEnd-faceY))*newHeight)+.15*newHeight);
+
+            }
+
+            Bitmap newGlasses = bp.createScaledBitmap(bp, (int)(.6*newWidth),
+                    (int) (.2*newHeight), true);
+
+
+            if(!(newMustache == null)) {
+                int mxShift = (int) ((newGlasses.getWidth() - newMustache.getWidth())/2);
+                int myShift = (int) (1 * newGlasses.getHeight());
+                int mX = gX - mxShift;
+                int mY = gY - myShift;
+                canvas.drawBitmap(newMustache,gX,gY,mMaskPaint);
+                canvas.drawBitmap(newGlasses,mX,mY,mMaskPaint);
+            }
+
+            // Draws Threshold Boxes
+            drawRect(xMargin, yMargin, (float) newWidth, (float) newHeight, mTargetPaint, canvas);
+            drawRect(xMarginH, yMarginH, (float) threshHihW, (float) threshHihH, mDeviationPaint, canvas);
+            drawRect(xMarginL, yMarginL, (float) threshLowW, (float) threshLowH, mDeviationPaint, canvas);
+
+
+
+            // Draws a circle at the position of the detected face, with the face's track id below.
+            float fx = translateX(face.getPosition().x + face.getWidth() / 2);
+            float y = translateY(face.getPosition().y + face.getHeight() / 2);
+            // Draws Text
+//        // Draws a bounding box around the face.
+            float xOffset = scaleX(face.getWidth() / 2.0f);
+            float yOffset = scaleY(face.getHeight() / 2.0f);
+            float left = fx - xOffset;
+            float top = y - yOffset;
+            float right = fx + xOffset;
+            float bottom = y + yOffset;
+            canvas.drawRect(left, top, right, bottom, mMaskPaint);
+
         }
+    }
 
-        double w1 = Math.sqrt(alpha/beta)*face.getWidth();
-        double h1 = Math.sqrt(alpha/beta)*face.getHeight();
-        int ow = (int) Math.sqrt(alpha/beta)*object.getWidth();
-        int oh = (int) Math.sqrt(alpha/beta)*object.getHeight();
+    private void drawRect(float mX, float mY, float w, float h, Paint p, Canvas c ){
+        c.drawLine(mX, mY, mX + w, mY, p);
+        c.drawLine(mX + w, mY, mX+w, mY +h, p);
+        c.drawLine(mX+w,mY+h,mX,mY+h,p);
+        c.drawLine(mX,mY+h,mX,mY,p);
+    }
 
-
-
-        //get landmarks
-        PointF LM = null;
-        PointF RM = null;
-        PointF N = null;
+    private void landMarkTester(Face face){
+        Log.i("face location", face.getPosition().x + "," + face.getPosition().y);
         for(Landmark landmark: face.getLandmarks()){
-            if(landmark.getType() == 5){ //leftmouth
-                LM = landmark.getPosition();
+            if(landmark.getType() == 4) {
+                Log.i("landmark",landmark.getPosition().x + "," + landmark.getPosition().y);
+                //  Log.i("face location", face.getPosition().x + "," + face.getPosition().y);
+                //Log.i("landmark translate", translateX(landmark.getPosition().x) + ", " + translateY(landmark.getPosition().y));
             }
-            if(landmark.getType() == 11){ //rightmouth
-                RM = landmark.getPosition();
+            if(landmark.getType() == 10) {
+                Log.i("landmark 10",landmark.getPosition().x + "," + landmark.getPosition().y);
+                //Log.i("face location 10", face.getPosition().x + "," + face.getPosition().y);
+               // Log.i("landmark translate 10", translateX(landmark.getPosition().x) + ", " + translateY(landmark.getPosition().y));
             }
-            if(landmark.getType() == 10) { //nose
-                N = landmark.getPosition();
-            }
-
         }
-
-        if(LM != null  && RM != null && N != null) {
-            double x1 = (face.getWidth() - (LM.x-RM.x))/face.getWidth();
-            double y1 = (face.getHeight() - N.y) / (face.getHeight());
-            double y2 = 1 - N.y / face.getHeight();
-
-            Log.i("xy nose ", N.x+" "+N.y);
-            Log.i("facepos", face.getPosition().x + " " + face.getPosition().y);
-            Log.i("proportions","x1: "+x1+" y1: "+y1+" y2: "+y2);
-
-            float xNew = (float) ((canvas.getWidth() - w1) / 2 + x1 * w1);
-            float yNew = (float) ((canvas.getHeight() - h1) / 2 + y1 * h1);
-            PointF UpperLeft = new PointF(xNew, yNew);
-
-            Log.i("upp", UpperLeft.x+ " " + UpperLeft.y);
-
-            int wf = (int) w1;
-            int hf = (int) h1;
-            Bitmap newImage = object.createScaledBitmap(object, ow, oh, true);
-            canvas.drawBitmap(object,translateX(UpperLeft.x),translateY(UpperLeft.y),mMaskPaint);
-
-            canvas.drawLine((float)((canvas.getWidth()-w1)/2), (float)((canvas.getHeight()-h1)/2),
-                    (float)((canvas.getWidth()-w1)/2), (float)(canvas.getHeight()-(canvas.getHeight()-h1)/2),
-                    mMaskPaint);
-        }
-        //test it out
-        //draw the new image
     }
-
 
 }
